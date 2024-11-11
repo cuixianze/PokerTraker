1967915d46f19b48165e069d5bf621872fe0a80d
JohnJung-1017 <jungih1017@gmail.com>
2024-11-11 17:12:11 +0900

    chore: npm install

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import MainPage from './pages/MainPage';
-import Navbar from './component/Navbar';
-import GameDetail from './pages/GameDetail';
-import Leaderboard from './pages/Leaderboard';
-import GameCreationForm from './pages/GameCreationForm';
-import UserForm from './pages/UserForm';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import Navbar from "./component/Navbar";
+import GameDetail from "./pages/GameDetail";
+import Leaderboard from "./pages/Leaderboard";
+import GameCreationForm from "./pages/GameCreationForm";
+import UserForm from "./pages/UserForm";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/games/:id" element={<GameDetail />} />
-          <Route path="/leaderboard" element={<Leaderboard />} />
-          <Route path="/create-game" element={<GameCreationForm />} />
-          <Route path="/create-user" element={<UserForm />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/games/:id" element={<GameDetail />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/create-game" element={<GameCreationForm />} />
+        <Route path="/create-user" element={<UserForm />} />
+      </Routes>
     </Router>
   );
 }
