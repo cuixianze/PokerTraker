9cd5cba02e4cc4340704e57ab965706100875c90
JohnJung-1017 <jungih1017@gmail.com>
2024-11-11 17:14:18 +0900

    feat: data
    main에 들어갈 데이터 컴포넌트 수정

---
+++ frontend/my-app/src/component/SharkFish.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SharkFish({ shark, fish }) {
+  return (
+    <div className="shark-fish">
+      <p>Shark: {shark}</p>
+      <p>Fish: {fish}</p>
+    </div>
+  );
+}
+
+export default SharkFish;
